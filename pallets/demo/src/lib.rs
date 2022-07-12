98d088819e288198d2995cf325db104c71a0c684
Nhuan Do <donhuanvn96@gmail.com>
2022-07-12 18:56:59 +0700

    done exercise of lesson-1

--- pallets/demo/src/lib.rs
+++ pallets/demo/src/lib.rs
@@ -21,14 +21,14 @@ use frame_system::pallet_prelude::*;
 #[frame_support::pallet]
 pub mod pallet {
 	pub use super::*;
-	pub type Id = u32;
+	pub type DNA = Vec<u8>;
 	#[derive(TypeInfo, Default, Encode, Decode)]
 	#[scale_info(skip_type_params(T))]
-	pub struct Students <T: Config> {
-		name: Vec<u8>,
-		age: u8,
+	pub struct Kitty<T: Config> {
+		dna: DNA,
+		owner: T::AccountId,
+		price: u32,
 		gender: Gender,
-		account: T::AccountId,
 	}
 
 	#[derive(TypeInfo, Encode, Decode)]
@@ -58,14 +58,18 @@ pub mod pallet {
 	// The pallet's runtime storage items.
 	// https://docs.substrate.io/v3/runtime/storage
 	#[pallet::storage]
-	#[pallet::getter(fn student_id)]
-	// Learn more about declaring storage items:
-	// https://docs.substrate.io/v3/runtime/storage#declaring-storage-items
-	pub type StudentId<T> = StorageValue<_, Id>;
+	#[pallet::getter(fn kitty_count)]
+	pub type KittyCount<T> = StorageValue<_, u32, OptionQuery>;
 
 	#[pallet::storage]
-	#[pallet::getter(fn student)]
-	pub(super) type Student<T: Config> = StorageMap<_, Blake2_128Concat, Id, Students<T>, OptionQuery>;
+	#[pallet::getter(fn kitty_map)]
+	pub(super) type KittyMap<T: Config> =
+		StorageMap<_, Blake2_128Concat, DNA, Kitty<T>, OptionQuery>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn owner_map)]
+	pub(super) type OwnerMap<T: Config> =
+		StorageMap<_, Blake2_128Concat, T::AccountId, Vec<DNA>, OptionQuery>;
 
 	// Pallets use events to inform users when important changes are made.
 	// https://docs.substrate.io/v3/runtime/events-and-errors
@@ -74,8 +78,8 @@ pub mod pallet {
 	pub enum Event<T: Config> {
 		/// Event documentation should end with an array that provides descriptive names for event
 		/// parameters. [something, who]
-		SomethingStored(u32, T::AccountId),
-		StudentStored(Vec<u8>, u8),
+		KittyStored(DNA, u32),
+		KittyOwnerChanged(DNA, T::AccountId, T::AccountId),
 	}
 
 	// Errors inform users that something went wrong.
@@ -84,8 +88,6 @@ pub mod pallet {
 		/// Error names should be descriptive.
 		NoneValue,
 		/// Errors should have helpful documentation associated with them.
-		StorageOverflow,
-		TooYoung
 	}
 
 	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
@@ -96,35 +98,78 @@ pub mod pallet {
 		/// An example dispatchable that takes a singles value as a parameter, writes the value to
 		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
 		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
-		pub fn create_student(origin: OriginFor<T>, name: Vec<u8>, age: u8) -> DispatchResult {
+		pub fn create_kitty(origin: OriginFor<T>, dna: DNA, price: u32) -> DispatchResult {
 			// Check that the extrinsic was signed and get the signer.
 			// This function will return an error if the extrinsic is not signed.
 			// https://docs.substrate.io/v3/runtime/origins
 			let who = ensure_signed(origin)?;
 
-			ensure!(age > 20, Error::<T>::TooYoung);
-
-			let gender = Self::generate_gender(name.clone())?;
-
-			let student = Students { 
-				name: name.clone(), 
-				age: age, 
-				gender: gender, 
-				account: who, 
+			// Automatically create gender from dna
+			let gender = match dna.len() {
+				0 => Gender::FEMALE,
+				_ => Gender::MALE,
 			};
 
-			// let current_id = Self::studend_id();
-			// let current_id = StudentId::<T>::get();
-			let current_id = <StudentId<T>>::get();
-			let mut current_id = current_id.unwrap_or(0);
+			let new_kitty: Kitty<T> = Kitty { dna: dna.clone(), owner: who.clone(), price, gender };
 
-			// Student::<T>::insert(current_id, student);
-			<Student<T>>::insert(current_id, student);
-			current_id += 1;
-			<StudentId<T>>::put(current_id);
+			// Store new kitty into KittyMap
+			<KittyMap<T>>::insert(dna.clone(), new_kitty);
+
+			// Update new count of kitty.
+			let mut count = <KittyCount<T>>::get().unwrap_or(0);
+			count += 1;
+			<KittyCount<T>>::put(count);
+
+			// Update OwnerMap at the pair of the key of this account id.
+			<OwnerMap<T>>::mutate(who.clone(), |query: &mut Option<Vec<DNA>>| {
+				match query {
+					Some(vector) => vector.push(dna.clone()),
+					None => *query = Self::generate_new_owner(dna.clone()),
+				};
+			});
 
 			// Emit an event.
-			Self::deposit_event(Event::StudentStored(name, age));
+			Self::deposit_event(Event::KittyStored(dna, price));
+			// Return a successful DispatchResultWithPostInfo
+			Ok(())
+		}
+
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
+		pub fn change_owner(
+			origin: OriginFor<T>,
+			dna: DNA,
+			new_owner: T::AccountId,
+		) -> DispatchResult {
+			// Check that the extrinsic was signed and get the signer.
+			// This function will return an error if the extrinsic is not signed.
+			// https://docs.substrate.io/v3/runtime/origins
+			let who = ensure_signed(origin)?;
+
+			// Update kitty's owner in KittyMap
+			<KittyMap<T>>::mutate(dna.clone(), |query: &mut Option<Kitty<T>>| match query {
+				Some(kitty) => kitty.owner = new_owner.clone(),
+				None => (),
+			});
+
+			// Update kitty vectors of related owners in OwnerMap
+			// Remove the kitty from the vector of the current owner.
+			<OwnerMap<T>>::mutate_exists(who.clone(), |query: &mut Option<Vec<DNA>>| match query {
+				Some(vector) => {
+					*vector = vector.drain(..).filter(|d| *d != dna).collect();
+					if vector.len() == 0 {
+						*query = None;
+					}
+				},
+				None => (),
+			});
+			// Add the kitty to the vector of the new owner.
+			<OwnerMap<T>>::mutate(new_owner.clone(), |query: &mut Option<Vec<DNA>>| match query {
+				Some(vector) => vector.push(dna.clone()),
+				None => *query = Self::generate_new_owner(dna.clone()),
+			});
+
+			// Emit an event.
+			Self::deposit_event(Event::KittyOwnerChanged(dna, who, new_owner));
 			// Return a successful DispatchResultWithPostInfo
 			Ok(())
 		}
@@ -133,8 +178,9 @@ pub mod pallet {
 
 // helper function
 impl<T> Pallet<T> {
-	fn generate_gender(name: Vec<u8>) -> Result<Gender, Error<T>> {
-		let res = if name.len() % 2 == 0 { Gender::FEMALE } else { Gender::MALE };
-		Ok(res)
+	fn generate_new_owner(dna: DNA) -> Option<Vec<DNA>> {
+		let mut new_vector = Vec::<DNA>::new();
+		new_vector.push(dna);
+		Some(new_vector)
 	}
 }
