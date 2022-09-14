65889907a2e7ec464e09abe4dea575adb26df28e
Nhuan Do <donhuanvn96@gmail.com>
2022-09-15 00:19:26 +0700

    clean code with FuncId enum

--- runtime/src/chain_extension.rs
+++ runtime/src/chain_extension.rs
@@ -12,6 +12,88 @@ use sp_runtime::DispatchError;
 #[derive(Default)]
 pub struct FetchRandomExtension;
 
+/// We're using enums for function IDs because contrary to raw u16 it enables
+/// exhaustive matching, which results in cleaner code.
+enum FuncId {
+	Random(HasSeed),
+	ReadAttributes(Attributes),
+	ReadDIDs(DIDs),
+}
+
+#[derive(Debug)]
+enum HasSeed {
+	Yes,
+	No,
+}
+
+#[derive(Debug)]
+#[derive(Default)]
+struct Attributes;
+
+#[derive(Debug)]
+#[derive(Default)]
+struct DIDs;
+
+impl TryFrom<u16> for FuncId {
+	type Error = DispatchError;
+
+	fn try_from(func_id: u16) -> Result<Self, Self::Error> {
+		let id = match func_id {
+			// Note: We use the first two bytes of interface selectors as function IDs,
+			// While we can use anything here, it makes sense from a convention perspective.
+			1100 => Self::Random(HasSeed::No),
+			1101 => Self::Random(HasSeed::Yes),
+			_ => {
+				error!("Called an unregistered `func_id`: {:}", func_id);
+				return Err(DispatchError::Other("Unimplemented func_id"));
+			},
+		};
+
+		Ok(id)
+	}
+}
+
+fn random<T, E>(func_id: HasSeed, env: Environment<E, InitState>) -> Result<(), DispatchError>
+where
+	T: pallet_contracts::Config + pallet_randomness_collective_flip::Config,
+	<E::T as SysConfig>::AccountId: UncheckedFrom<<E::T as SysConfig>::Hash> + AsRef<[u8]>,
+	E: Ext<T = T>,
+{
+	let mut env = env.buf_in_buf_out();
+	let arg: [u8; 32] = env.read_as()?;
+	let random_seed = <pallet_randomness_collective_flip::Pallet<T> as Randomness<
+		T::Hash,
+		T::BlockNumber,
+	>>::random(&arg)
+	.0;
+	let random_slice = random_seed.encode();
+	trace!(target: "runtime", "[ChainExtension]|call|func_id:{:?}", func_id);
+	env.write(&random_slice, false, None)
+		.map_err(|_| DispatchError::Other("ChainExtension failed to call random"))?;
+	Ok(())
+}
+
+fn read_attributes<T, E>(
+	_attrs: Attributes,
+	_env: Environment<E, InitState>,
+) -> Result<(), DispatchError>
+where
+	T: pallet_contracts::Config + pallet_randomness_collective_flip::Config,
+	<E::T as SysConfig>::AccountId: UncheckedFrom<<E::T as SysConfig>::Hash> + AsRef<[u8]>,
+	E: Ext<T = T>,
+{
+	Ok(())
+}
+
+fn read_dids<T, E>(_dids: DIDs, _env: Environment<E, InitState>) -> Result<(), DispatchError>
+where
+	T: pallet_contracts::Config + pallet_randomness_collective_flip::Config,
+	<E::T as SysConfig>::AccountId: UncheckedFrom<<E::T as SysConfig>::Hash> + AsRef<[u8]>,
+	E: Ext<T = T>,
+{
+	Ok(())
+}
+
 impl<T> ChainExtension<T> for FetchRandomExtension
 where
 	T: pallet_contracts::Config + pallet_randomness_collective_flip::Config,
@@ -22,26 +104,12 @@ where
 		E: Ext<T = T>,
 		<E::T as SysConfig>::AccountId: UncheckedFrom<<E::T as SysConfig>::Hash> + AsRef<[u8]>,
 	{
-		let func_id = env.func_id();
+		let func_id = FuncId::try_from(env.func_id())?;
 		match func_id {
-			1101 => {
-				let mut env = env.buf_in_buf_out();
-				let arg: [u8; 32] = env.read_as()?;
-				let random_seed = <pallet_randomness_collective_flip::Pallet<T> as Randomness<
-					T::Hash,
-					T::BlockNumber,
-				>>::random(&arg)
-				.0;
-				let random_slice = random_seed.encode();
-				trace!(target: "runtime", "[ChainExtension]|call|func_id:{:}", func_id);
-				env.write(&random_slice, false, None)
-					.map_err(|_| DispatchError::Other("ChainExtension failed to call random"))?;
-			},
-			_ => {
-				error!("Called an unregistered `func_id`: {:}", func_id);
-				return Err(DispatchError::Other("Unimplemented func_id"));
-			},
-		};
+			FuncId::Random(func_id) => random::<T, E>(func_id, env)?,
+			FuncId::ReadAttributes(attrs) => read_attributes::<T, E>(attrs, env)?,
+			FuncId::ReadDIDs(dids) => read_dids::<T, E>(dids, env)?,
+		}
 		Ok(RetVal::Converging(0))
 	}
 
